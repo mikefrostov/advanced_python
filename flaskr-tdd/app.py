8fd9d07302611f0bf7993d86d78c5e23b941e243
mikefrostov <m.e.morozov1@gmail.com>
2019-09-27 13:59:12 +0300

    404 test

--- flaskr-tdd/app.py
+++ flaskr-tdd/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, jsonify
+
+# configuration
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'my_precious'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
 
 if __name__ == '__main__':
