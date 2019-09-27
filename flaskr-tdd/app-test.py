f964bc836190ccab02b4586c129bbf4fe59724a2
mikefrostov <m.e.morozov1@gmail.com>
2019-09-27 14:11:13 +0300

    test of init_db

--- flaskr-tdd/app-test.py
+++ flaskr-tdd/app-test.py
@@ -1,5 +1,5 @@
 from app import app
-
+import os
 import unittest
 
 
@@ -9,6 +9,10 @@ class BasicTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 404)
+
+    def test_database(self):
+        tester = os.path.exists("flaskr.db")
+        self.assertTrue(tester)
 
 
 if __name__ == '__main__':
